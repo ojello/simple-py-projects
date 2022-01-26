95f7a0de1d394c00b275be8da82ada3f9f8545f9
Kist <kbear.swe@gmail.com>
2022-01-25 20:12:04 -0600

    added random list generator

--- CoinFlipGame.py
+++ CoinFlipGame.py
@@ -1,18 +1,32 @@
-# write a program to find out how ofte na streak of six heads or a streak of six tails come up in a randomly generated list of heads and tails
+# CoinFlip Game is a program to find out how often a streak of six heads or a streak of six tails come up in a randomly generated list of heads and tails
 
 # first first part: generate a list of randomly selected 'heads' and 'tails' values
 # second part: checks if there is a streak in it
 # put all of this code in a loop that repeats the experiement 10,000 times so we can find out what percentage of the coin flips contain a streak of six heads or tails in a row.
 
 # the function call random.randint(-0, 1) will return a o value of 50% of the time and a 1 value the other 50% of the time.
-
 import random
-
 numberOfStreaks = 0
+
 for experimentNumber in range(10000):
+    random_list = []
+    for i in range(1, 101):
+        # return a value of 0 50% and 1 50% of the time
+        randomNumber = random.randint(0, 1)
+        if randomNumber == 1:
+            computer_choice = 'H'
+            random_list = random_list + [computer_choice]
+        elif randomNumber == 0:
+            computer_choice = 'T'
+            random_list = random_list + [computer_choice]
+print(random_list)
+# for i in random_list:
+#     print('hello')
 
-    # Code that creates a list of 100 'heads' or 'tails' values.
 
-    # Code that checks if there is a streak of 6 heads or tails in a row.
-print('Chance of streak: %s%%' %
-      (numberOfStreaks / 100))
+# Code that checks if there is a streak of 6 heads or tails in a row
+
+
+# for experimentNumber in range(10000):
+# Code that checks if there is a streak of 6 heads or tails in a row.
+# print('Chance of streak: %s%%' % (numberOfStreaks / 100))
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,5 +1,1 @@
-
-# while True:
-#     print(str(win) + ' Wins' + str(loss) + ' Losses' + str(tie) + ' Ties')
-#     user_input = input(
-#         'Enter your move: r(ock) (p)aper (s) scissors or (q)uit game')
+print(random_list)
